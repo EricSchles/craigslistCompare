89078c40885c43718d064d6ead17f8278fadb950
EricSchles <ericschles@gmail.com>
2014-10-30 18:40:50 -0700

    things

--- grab.py
+++ grab.py
@@ -5,5 +5,10 @@ class Scraper:
     def __init__(self):
         pass
     
+    def get_nynj_backpages(self):
+        ny_nj_backpage = ["newyork","cnj","jerseyshore","newjersey","southjersey"]
     def grab(self):
-        pass
+        
+    
+    
+    
