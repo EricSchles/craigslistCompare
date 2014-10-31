25784e7e9dde70e0ebfda00703beaac2db8aae85
EricSchles <ericschles@gmail.com>
2014-10-30 18:51:22 -0700

    grabbing things

--- grab.py
+++ grab.py
@@ -1,29 +1,15 @@
 import requests
 import lxml.html
 import pickle
+
 class Scraper:
     def __init__(self):
         pass
     
-    def get_nynj_craigslist(self):
-        ny_nj_craigslist = ["newyork","cnj","jerseyshore","newjersey","southjersey"]
-        r = requests.get("https://www.craigslist.org/about/sites")
-        html = lxml.html.fromstring(r.text)
-        craigslists = html.xpath("//a/@href")
-        links = []
-        for i in craigslists:
-            if "craigslist" in i:
-                for area in ny_nj_craigslist:
-                    if area in i:
-                        if not "www" in i:
-                            i = str(i)
-                            links.append(i)
-        with open("nynj_craigslists","w") as f:
-            pickle.dump(links,f)
-
+      
 
     def grab(self):
-        
+        craigslists= ["http://newyork.craigslist.org/","http://cnj.craigslist.org/","http://jerseyshore.craigslist.org/","http://newjersey.craigslist.org/","http://southjersey.craigslist.org/"]
     
     
     
