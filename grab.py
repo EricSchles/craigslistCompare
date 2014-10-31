7b6181b28667d62027cdebe888de0a0501eab0d4
EricSchles <ericschles@gmail.com>
2014-10-30 19:10:00 -0700

    adding grequests

--- grab.py
+++ grab.py
@@ -1,22 +1,26 @@
 import requests
 import lxml.html
 import pickle
+import grequests
 
 class Scraper:
     def __init__(self):
         pass
 
-    def grab(self):
+    def setup(self):
         craigslists= ["http://newyork.craigslist.org/","http://cnj.craigslist.org/","http://jerseyshore.craigslist.org/","http://newjersey.craigslist.org/","http://southjersey.craigslist.org/"]
         casual_encounters = []
         for site in craigslists:
             casual_encounters.append(site+"search/w4m")
-        indexed_sites = []
+        urls = []
         for site in casual_encounters:
             for i in xrange(15):
                 if i == 0:
-                    indexed_sites.append(site)
+                    urls.append(site)
                 else:
-                    indexed_sites.append(site+"?s="+str(i)+"00&")
-        
-    
+                    urls.append(site+"?s="+str(i)+"00&")
+        rs = (grequests.get(u) for u in urls)
+        responses = grequests.map(rs)
+        for r in responses:
+            
+                    
