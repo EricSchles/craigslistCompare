56cda105515a303c3f6b66d16e9814d9a4cee40b
EricSchles <ericschles@gmail.com>
2014-10-30 19:48:45 -0700

    fixing bugs

--- grab.py
+++ grab.py
@@ -7,14 +7,14 @@ class Scraper:
     def __init__(self):
         pass
 
-    def setup(self):
+    def setup(self,num_pages=2):
         craigslists= ["http://newyork.craigslist.org/","http://cnj.craigslist.org/","http://jerseyshore.craigslist.org/","http://newjersey.craigslist.org/","http://southjersey.craigslist.org/"]
         casual_encounters = []
         for site in craigslists:
             casual_encounters.append(site+"search/w4m")
         urls = []
         for site in casual_encounters:
-            for i in xrange(15):
+            for i in xrange(num_pages):
                 if i == 0:
                     urls.append(site)
                 else:
@@ -30,17 +30,19 @@ class Scraper:
                     ads.append("http://"+r.url.split("/")[-3]+link)
         return ads
 
-    def parse(self,link):
+    def parse(self,r):
         values = {}
-        html = lxml.html.fromstring(r.text)
-        title = html.xpath('//h2[@class="postingtitle"]')
-        body = html.xpath('//section[@id="postingbody"]')
+        text = r.text.encode("ascii","ignore")
+        html = lxml.html.fromstring(text)
+        values["title"] = html.xpath('//h2[@class="postingtitle"]')
+        values["body"] = html.xpath('//section[@id="postingbody"]')
+        return values
 
     def run(self):
         ads = self.setup()
         df = pd.DataFrame()
         rs = (grequests.get(u) for u in ads)
         responses = grequests.map(rs)
-        for r in responses:
-            self.parse(r.text)
+        for r in responses[:2]:
+            print self.parse(r)
             
